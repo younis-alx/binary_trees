d5e5946392984142fc34f98a4d21d5c8c377e2e7
jay-alx <126336909+younis-alx@users.noreply.github.com>
2023-08-29 13:33:41 +0000

    update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,7 @@
 #ifndef TREES
 #define TREES
+
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,5 +19,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 
 #endif
