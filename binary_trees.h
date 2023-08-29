7fe2b1acac6e90655a3ea9bc0be8effdc9da9a5e
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-08-29 06:55:23 -0700

    added lib

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef TREES
 #define TREES
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,6 +21,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
