745e52d08f8fae1ebb2fbc6e7d0bf1b9ea53ee09
Younis ahmed <younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.comgit config --global user.email younisalx-swe@hotmail.com>
2023-08-31 04:04:30 -0700

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
-/*
+/**
 * binary_tree_is_leaf - check if node is leaf
 * @node: node to check
+* Return: true or false
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
